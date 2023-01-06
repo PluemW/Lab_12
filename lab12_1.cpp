c96c289bcba3637b42e2dbdb8441e14d25ff4867
PluemW <jaroenwachirasak@gmail.com>
2023-01-06 16:14:45 +0700

    Filled

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,25 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int ran;
+string grade;
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    ran = rand()%9;
+    if(ran == 0) grade="A";
+    else if(ran == 1) grade="B+";
+    else if(ran == 2) grade="B";
+    else if(ran == 3) grade="C+";
+    else if(ran == 4) grade="C";
+    else if(ran == 5) grade="D+";
+    else if(ran == 6) grade="D";
+    else if(ran == 7) grade="F";
+    else if(ran == 8)grade="W";
+    else grade = "Error";
+    cout << "You will get "<<grade<<" in this 261102.";
+}
